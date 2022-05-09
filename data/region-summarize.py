042c7f0663a6d57edb9a3f46a4bb8f5894f00899
Femboy Compooter <theophile.fouillet@protonmail.com>
2022-05-09 21:35:19 +0200

    Avancée sur le script

--- data/region-summarize.py
+++ data/region-summarize.py
@@ -2,7 +2,7 @@ import csv
 import json
 import string
 from unittest import result
-
+from time import sleep
 i=0
 
 resultats = {  
@@ -13,12 +13,11 @@ with open("2tour2022.csv", "r") as csvfile:
         vainqueur=1
         if(row[2]!="Code de la commune"):
             if(row[23] < row[30]):
-                vainqueur=2
                 resultats[i] = {
                     "code-dpt": row[0],
                     "voix-1": row[23],
                     "voix-2": row[30],
-                    "vainqueur":1
+                    "vainqueur":2
                 }
             if(row[23] == row[30]):
                 resultats[i] = {
@@ -27,15 +26,40 @@ with open("2tour2022.csv", "r") as csvfile:
                     "voix-2": row[30],
                     "vainqueur":0
                 }
+            if(row[23] > row[30]):
+                vainqueur=1
+                resultats[i] = {
+                    "code-dpt": row[0],
+                    "voix-1": row[23],
+                    "voix-2": row[30],
+                    "vainqueur":1
+                }
+
             i+=1
     csvfile.close()
     with open('output/dpts.json', 'w') as ouptputfile:
-        resultats_write = json.dumps(resultats)
+        resultats_write = json.dumps(resultats, separators=(',', ':'))
         ouptputfile.write(resultats_write)
         ouptputfile.close()
-    
 
 regions = { }
 i=0
+
+current_dpt="01"
+votes_1=0
+votes_2=0
 for key in resultats:
-    print(key)
+    if(resultats[key]["code-dpt"]==current_dpt):
+        votes_1+=int(resultats[key]["voix-1"])
+        votes_2+=int(resultats[key]["voix-2"])
+
+    if(resultats[key]["code-dpt"]!=current_dpt):
+        regions[current_dpt] = {
+            "voix-1": votes_1,
+            "voix-2": votes_2
+        }
+        current_dpt=resultats[key]["code-dpt"]
+        with open('output/dpts-clean.json', 'w') as ouptputfile:
+            resultats_write = json.dumps(regions, separators=(',', ':'))
+            ouptputfile.write(resultats_write)
+            ouptputfile.close()
