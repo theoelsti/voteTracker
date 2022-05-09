f168df2b4cc7d0e7cd7e62a5df1a182c99e40276
Femboy Compooter <theophile.fouillet@protonmail.com>
2022-05-09 22:35:36 +0200

    avancée sur la recuperation des codes régions

--- app/script/fr-map.js
+++ app/script/fr-map.js
@@ -4,8 +4,26 @@ var map = new Datamap({
     element: document.getElementById('franceMap'),
     scope:"fra",
     geographyConfig: {
-    dataJson: "script/fra.topo.json"
+    dataJson: "script/fra.topo.json",
+    popupTemplate: function(geography, data) {
+        return '<div class="hoverinfo"> <b>' + geography.properties.name + '</b> '+ data.votes + ' votes</div>';
+      }
  },
+ fills: {
+    'EMacron': '#022598',
+    'MLepen': '#0391e3',
+    defaultFill: '#b5caf9'
+  },
+
+
+
+data:{
+
+    "FR.YV": {
+        "fillKey": "EMacron",
+        "votes":69
+    }
+},
 setProjection: function(element) {
     var projection = d3.geo.mercator()
         .scale(2000)
--- data/region-summarize.py
+++ data/region-summarize.py
@@ -13,34 +13,26 @@ with open("2tour2022.csv", "r") as csvfile:
         vainqueur=1
         if(row[2]!="Code de la commune"):
             if(row[23] < row[30]):
-                resultats[i] = {
-                    "code-dpt": row[0],
-                    "voix-1": row[23],
-                    "voix-2": row[30],
-                    "vainqueur":2
-                }
+                vainqueur=2
             if(row[23] == row[30]):
-                resultats[i] = {
-                    "code-dpt": row[0],
-                    "voix-1": row[23],  
-                    "voix-2": row[30],
-                    "vainqueur":0
-                }
+                vainqueur=0
             if(row[23] > row[30]):
                 vainqueur=1
-                resultats[i] = {
-                    "code-dpt": row[0],
-                    "voix-1": row[23],
-                    "voix-2": row[30],
-                    "vainqueur":1
-                }
-
+            resultats[i] = {
+                "code-dpt": row[0],
+                "libelle-dpt": row[1],
+                "voix-1": row[23],
+                "voix-2": row[30],
+                "vainqueur":vainqueur
+            }
             i+=1
+
     csvfile.close()
-    with open('output/dpts.json', 'w') as ouptputfile:
-        resultats_write = json.dumps(resultats, separators=(',', ':'))
-        ouptputfile.write(resultats_write)
-        ouptputfile.close()
+
+with open('output/dpts.json', 'w') as ouptputfile:
+    resultats_write = json.dumps(resultats, separators=(',', ':'))
+    ouptputfile.write(resultats_write)
+    ouptputfile.close()
 
 regions = { }
 i=0
@@ -53,13 +45,42 @@ for key in resultats:
         votes_1+=int(resultats[key]["voix-1"])
         votes_2+=int(resultats[key]["voix-2"])
 
-    if(resultats[key]["code-dpt"]!=current_dpt):
+    if(resultats[key]["code-dpt"]!=current_dpt or key==len(resultats)-1):
+        if(votes_1>votes_2):
+            vainqueur=1
+        if(votes_1==votes_2):
+            vainqueur=0
+        if(votes_1<votes_2):
+            vainqueur=2
         regions[current_dpt] = {
             "voix-1": votes_1,
-            "voix-2": votes_2
+            "voix-2": votes_2,
+            "libelle-dpt": resultats[key-1]["libelle-dpt"],
+            "vainqueur":vainqueur
         }
         current_dpt=resultats[key]["code-dpt"]
-        with open('output/dpts-clean.json', 'w') as ouptputfile:
+    i+=1
+
+with open('output/dpts-clean.json', 'w') as ouptputfile:
             resultats_write = json.dumps(regions, separators=(',', ':'))
             ouptputfile.write(resultats_write)
-            ouptputfile.close()
+            ouptputfile.close()       
+
+
+regions = {
+
+    "01":"FR.AI",
+    "02":"FR.AS",
+    "03":"FR.AL",
+    "04":"FR.AP",
+    "05":"FR.AM",
+    "06":"FR.AH"
+}
+
+
+with open('../app/script/fra.topo.json', 'r') as topofile:
+    data = topofile.read()
+    data=json.loads(data)
+    topofile.close()
+for key in data["objects"]["fra"]:
+    print(key["geometries"])
