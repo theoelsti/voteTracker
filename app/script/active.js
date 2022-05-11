d2a7e03a0ecac87040a27c0c5a28b27b59bbc77f
Femboy Compooter <theophile.fouillet@protonmail.com>
2022-05-11 22:13:13 +0200

    debut export

--- app/script/active.js
+++ app/script/active.js
@@ -1,2 +1,4 @@
 let filename = window.location.pathname.split("/").pop().split(".")[0];
+if(filename == "index" || filename == "votes"){
 document.getElementById(filename).className ="nav-link active";
+}
