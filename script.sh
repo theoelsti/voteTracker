8571862700d704f7b0e6cd29841b353ebf97c86b
Femboy Compooter <theophile.fouillet@protonmail.com>
2022-05-12 09:50:18 +0200

    update

--- script.sh
+++ script.sh
@@ -1,17 +1,32 @@
-echo "Mise a jour..."
+#!/bin/bash
+#63TEFT
+#Script permettant d'installer les prérequis 
+# Met un disclaimer a l'ecran
+echo "
+############################################################################################
+#                                      /!\ ATTENTION ! /!\                                 #
+#   Le script est sucéptible d'installer des mises à jour, pouvant consommer + 200 Mo      #
+#                                                                                          #
+#                                   Souhaitez-vous continuer ?                             #
+############################################################################################
+
+"
+read -p "Oui (o) ou non (n) : " reponse
+# Verifie si la réponse est non
+if [ $reponse = "n" ]
+then
+    echo "Au revoir !"
+    exit 2
+fi
+
 sudo apt update && sudo apt upgrade
-echo "Installation de apache..."
 sudo apt install apache2 -y
-echo "Installation de php..."
 sudo apt install php -y
-echo "Installation de mysql..."
 sudo apt install mysql-server -y
-echo "Installation de phpmyadmin..."
 sudo apt install phpmyadmin -y
-echo "Installation de git..."
 sudo apt install git -y
-echo "Création du certificat..."
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout private.key -out certificate.crt
-echo "Certificat crée... Déplacement en cours..."
 sudo mv private.key /etc/ssl/private/
 sudo mv certificate.crt /etc/ssl/certs/
+
+exit 0
